3b7da9f9c9a85e6ac65dfdfcefef989e33b6b9f4
fadich <fadi.ahmad@wdg.com.ua>
2017-05-15 17:16:38 +0300

    ``` frontend > nav bar > more apps

--- frontend/app/navigation/header-nav.component.ts
+++ frontend/app/navigation/header-nav.component.ts
@@ -1,8 +1,18 @@
 import {Component} from 'angular2/core';
+import { MoreAppsService } from './more-apps.service';
 
 @Component({
     selector: 'header',
-    templateUrl: 'src/templates/navigation/header-nav.html'
+    templateUrl: 'src/templates/navigation/header-nav.html',
+    providers: [
+        MoreAppsService
+    ]
 })
 
-export class HeaderNavComponent { }
+export class HeaderNavComponent {
+    public moreApps:MoreAppsService;
+
+    constructor () {
+        this.moreApps = new MoreAppsService();
+    }
+}
