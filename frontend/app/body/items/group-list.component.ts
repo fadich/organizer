1c699c066d3d19c7f339da98a64ea19416c5603b
fadich <fadi.ahmad@wdg.com.ua>
2017-05-19 13:35:33 +0300

    ``` frontend > item actions

--- frontend/app/body/items/group-list.component.ts
+++ frontend/app/body/items/group-list.component.ts
@@ -1,12 +1,16 @@
 import { Component } from 'angular2/core';
 import { TodoItemService } from './todo-item.service';
+import { ActionsComponent } from './actions.component';
 
 @Component({
     selector: '[id=main]',
     templateUrl: 'src/templates/body/items/list-group-items.html',
     providers: [
-        TodoItemService
+        TodoItemService,
     ],
+    directives: [
+        ActionsComponent,
+    ]
 })
 
 export class GroupListComponent {
