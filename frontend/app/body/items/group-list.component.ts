6288ba9d33b359641882063387f13fd4c09f0129
fadich <fadi.ahmad@wdg.com.ua>
2017-05-19 13:16:14 +0300

    ``` frontend > item list

--- frontend/app/body/items/group-list.component.ts
+++ frontend/app/body/items/group-list.component.ts
@@ -1,8 +1,17 @@
 import { Component } from 'angular2/core';
+import { TodoItemService } from './todo-item.service';
 
 @Component({
     selector: '[id=main]',
     templateUrl: 'src/templates/body/items/list-group-items.html',
+    providers: [
+        TodoItemService
+    ],
 })
 
-export class GroupListComponent { }
+export class GroupListComponent {
+
+    public constructor (
+        public todoItemService:TodoItemService
+    ) {  }
+}
---
+++ frontend/app/body/items/todo-item.service.ts
@@ -0,0 +1,72 @@
+import { Injectable } from 'angular2/core';
+import { TodoItem } from '../../entities/todo-item';
+
+@Injectable()
+
+export class TodoItemService {
+    protected items:TodoItem[] = [];
+
+    public getItems():TodoItem[] {
+        if (!this.items.length) {
+            let items = TodoItemService.requestItems();
+            for (let item of items) {
+                this.items.push(
+                    new TodoItem(
+                        item['id'],
+                        item['title'],
+                        item['content'],
+                        item['status'],
+                        item['userId'],
+                        item['createdAt'],
+                        item['updatedAt'],
+                    )
+                )
+            }
+        }
+
+        return this.items;
+    }
+
+    protected static requestItems():object[] {
+        return [
+            {
+                id: 1,
+                title: "Some item",
+                content: "Item content........",
+                status: 2,
+                userId: 0,
+                createdAt: 666,
+                updatedAt: 777,
+            },
+            {
+                id: 2,
+                title: "Second item",
+                content: "Item contentItem contentItem contentItem contentItem contentItem content" +
+                          "Item contentItem contentItem contentItem contentItem contentItem content" +
+                          "Item contentItem contentItem contentItem contentItem contentItem content",
+                status: 4,
+                userId: 0,
+                createdAt: 123456,
+                updatedAt: 1234567,
+            },
+            {
+                id: 5,
+                title: "Fifth item",
+                content: null,
+                status: 4,
+                userId: 0,
+                createdAt: 0,
+                updatedAt: 99999999,
+            },
+            {
+                id: 10,
+                title: "Tenth item",
+                content: "Tenth item content.",
+                status: 3,
+                userId: 0,
+                createdAt: 1111111666,
+                updatedAt: 2222222777,
+            },
+        ];
+    }
+}
